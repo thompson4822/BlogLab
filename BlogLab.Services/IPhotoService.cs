20b232232ac21013e04d7f9995e19c97cf7eee80
Steve Thompson <thompson4822@protonmail.com>
2021-07-27 21:37:30 -0600

    Starting to get Cloudinary integrated

---
+++ BlogLab.Services/IPhotoService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlogLab.Models.Settings;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace BlogLab.Services
+{
+    public interface IPhotoService
+    {
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+
+    class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public PhotoService(IOptions<CloudinaryOptions> config)
+        {
+            var account = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
